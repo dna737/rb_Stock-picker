b697a2c549c125b22337909b6f54d46a971097d9
dna737 <adnanhere27@gmail.com>
2022-11-28 08:15:35 +0530

    return array that replicates purchasing behavior

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,13 @@
-arr = []
 def stock_picker(prices)
-    
+    arr = Array.new(0)
+    prices.each do |price|
+        arr.push(modify_prices(prices, price))
+    end
+    arr
 end
 
-def modified_array
-
+def modify_prices(prices, price)
+    prices.map{|element| element-price}
 end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])

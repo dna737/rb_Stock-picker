b93c1d3805baf167b65b9df5f7d1b3a145a800a5
dna737 <adnanhere27@gmail.com>
2022-11-28 16:08:18 +0530

    implement reverse sort

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,7 +2,18 @@ def stock_picker(prices)
     profits = {}
     sorted_prices = prices.sort
     sorted_prices.each do|price| 
-        
+        index = prices.find_index(price)
+        #We found the index. Find the highest number after that index.
+        #this index represents the day we "bought" the stock.
+        unless index == prices.size
+            #take the decreasing prices:
+            decreasing_prices = prices.sort.reverse
+            decreasing_prices.each do|highest|
+                if prices.find_index(higest) > index then 
+                    
+                end
+            end
+        end
     end
 end
 

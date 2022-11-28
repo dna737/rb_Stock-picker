ffb6d4c749737e0c5658ff628f7aed2b1807b94b
dna737 <adnanhere27@gmail.com>
2022-11-28 15:56:11 +0530

    start with sorting approach for stock prices

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,8 @@
 def stock_picker(prices)
-    arr = Array.new(0)
-    prices.each do |price|
-        arr.push(modify_prices(prices, price))
+    profits = {}
+    sorted_prices = prices.sort
+    sorted_prices.each do|price| 
+        
     end
-    arr
 end
 
-def modify_prices(prices, price)
-    prices.map{|element| element-price}
-end
-
-p stock_picker([17,3,6,9,15,8,6,1,10])

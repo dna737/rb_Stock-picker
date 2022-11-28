d2d162b7672deb6277f2c9f7978ecc6c8784aa13
dna737 <adnanhere27@gmail.com>
2022-11-28 16:23:58 +0530

    implement hashtable to store profits

--- stock_picker.rb
+++ stock_picker.rb
@@ -9,11 +9,19 @@ def stock_picker(prices)
             #take the decreasing prices:
             decreasing_prices = prices.sort.reverse
             decreasing_prices.each do|highest|
-                if prices.find_index(higest) > index then 
-                    
+                if prices.find_index(highest) > index then 
+                    difference = highest-price
+                    unless profits.has_key?(difference.to_sym)
+                    profits[difference] = Array.new(0).push(index).push(prices.find_index(higest))
+                    end
                 end
             end
         end
     end
+    #After we've implemented that for each stock, find the maximum key and return its corresponding value
+    profits.max_by(&:last)
+    #Credit to @mahemoff for the statement above: https://stackoverflow.com/questions/6040494/how-to-find-the-key-of-the-largest-value-hash#comment52054725_6040733
 end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
 

52795d39655d7ec73f8fde64e52596f734e508ce
dna737 <adnanhere27@gmail.com>
2022-12-02 01:37:15 +0530

    implement no-profit case

--- stock_picker.rb
+++ stock_picker.rb
@@ -27,6 +27,10 @@ def stock_picker(prices)
 end
 
 def new_stock_picker(prices)
+    if prices == prices.sort.reverse then
+        return 0 #no point in trading if you won't have any profit
+    end
+
     
 end
 p stock_picker([17,3,6,9,15,8,6,1,10])

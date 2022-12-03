160abc71a821bad4e15f49685f33d67afa751a37
dna737 <adnanhere27@gmail.com>
2022-12-03 15:25:18 +0530

    test one more input; return correct hash

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,52 +1,4 @@
 def stock_picker(prices)
-    if prices == prices.sort.reverse then
-        return 0 #since there's no point in buying stocks now
-    end 
-    profits = {}
-    sorted_prices = prices.sort
-    sorted_prices.each do|price| 
-        index = prices.find_index(price)
-        #We found the index. Find the highest number after that index.
-        #this index represents the day we "bought" the stock.
-        unless index == prices.size
-            #take the decreasing prices:
-            decreasing_prices = prices.sort.reverse
-            decreasing_prices.each do|highest|
-                if prices.find_index(highest) > index then 
-                    difference = highest-price
-                    unless profits.has_key?(difference)
-                    profits[difference] = Array.new(0).push(index).push(prices.find_index(highest))
-                    end
-                end
-            end
-        end
-    end
-    #After we've implemented that for each stock, find the maximum key and return its corresponding value
-    profits.max_by(&:first).last
-    #Credit to @mahemoff for the statement above: https://stackoverflow.com/questions/6040494/how-to-find-the-key-of-the-largest-value-hash#comment52054725_6040733
-end
-
-def new_stock_picker(prices)
-    if prices == prices.sort.reverse then
-        return 0 #no point in trading if you won't have any profit
-    end
-
-    sorted_prices = prices.sort
-    reverse_prices = prices.sort.reverse
-    result = Array.new(0)
-    sorted_prices.each do|curr_price|
-        reverse_prices.each do|highest|
-            if(prices.find_index(curr_price) < prices.find_index(highest)) then
-                if(result[0] != nil) then 
-                    result.pop                    
-                end
-                result = Array.new(0).push(highest - curr_price).push(Array.new(0).push(prices.find_index))
-            end
-        end
-    end
-end
-
-def stock_picker_3(prices)
     if(prices == prices.sort.reverse) then return "This is not the time to buy stocks :(" end
     result = {}
     prices.each_with_index do|price, i|
@@ -55,8 +7,13 @@ def stock_picker_3(prices)
         result[subarray.max - price] = [i, prices.find_index(subarray.max)] #storing the array of indices in the hash
     end
     #send the value corresponding to the highest key by uing hash[hash.keys.max]
-    
+    result[result.keys.max]
 end
 
-p new_stock_picker([17,3,6,9,15,8,6,1,10])
+puts "Case #1:"
+p stock_picker([17,3,6,9,15,8,6,1,10])
+#Outputs [1,4] because that's when the profit is the highest ($15 -  $3 == $12)
 
+puts "\nCase #2:"
+p stock_picker([9,7,3,2,4,8,1,3])
+#Outputs [3,5] because that's when the profit is the highest ($8 - $2 == $6)
